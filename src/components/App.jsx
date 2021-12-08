a45f5741d0ae83a83731ea147493eba3737ac113
SHINEILCHI <neilpatrickelison.npe@gmail.com>
2021-12-08 16:48:42 +0800

    Changed API endpoints to the newly named app server from heroku.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,13 +11,13 @@ function App(props) {
 
   // get the current notes inside the database
   useEffect(() => {
-    Axios.get("https://salty-savannah-57698.herokuapp.com/").then((response) => {
+    Axios.get("https://keeper-app-clone-mern-stack.herokuapp.com/").then((response) => {
       setNoteItems(response.data)
     })
   }, [])
 
   async function refreshPage() {
-    const response = await Axios.get("https://salty-savannah-57698.herokuapp.com/");
+    const response = await Axios.get("https://keeper-app-clone-mern-stack.herokuapp.com/");
     
     if (response.status === 200){
       setNoteItems(response.data);
@@ -27,7 +27,7 @@ function App(props) {
   }
 
   async function addNote(inputContent, setInputContent) {
-    const response = await Axios.post("https://salty-savannah-57698.herokuapp.com/createNote", {
+    const response = await Axios.post("https://keeper-app-clone-mern-stack.herokuapp.com/createNote", {
       title: inputContent.title, 
       content: inputContent.content
     });
@@ -45,7 +45,7 @@ function App(props) {
   }
 
   async function deleteNote(_id) {
-    const response = await Axios.delete("https://salty-savannah-57698.herokuapp.com/deleteNote", {params: { _id }});
+    const response = await Axios.delete("https://keeper-app-clone-mern-stack.herokuapp.com/deleteNote", {params: { _id }});
     if (response.status === 200) {
       setNoteItems((prevNoteItems) => {
         return prevNoteItems.filter((note) => {
