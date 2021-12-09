ef633092e9b204b68771d647d29c4d2e5d88ff0b
SHINEILCHI <neilpatrickelison.npe@gmail.com>
2021-12-09 12:46:58 +0800

    Build_v1

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("./build"));
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5500;
+}
+app.listen(port, () => {
+    console.log("Server is running at port: 5000");
+})
